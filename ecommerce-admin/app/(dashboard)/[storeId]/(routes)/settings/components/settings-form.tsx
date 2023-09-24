177ddd5b4a00a4149795c1dda8a887a4201a3adf
George Lungu <george.lungu1896@gmail.com>
2023-09-24 12:47:20 +0300

    created routes to patch and delete stores in admin dashboard

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,13 +1,16 @@
 "use client"
 
-// LEFT AT 03:00:05
+// LEFT AT 03:15:26
 
 import * as z from "zod"
+import axios from "axios";
+import toast from "react-hot-toast";
 import { Store } from "@prisma/client"
 import { Trash } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useParams, useRouter } from "next/navigation";
 
 import { Heading } from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
@@ -22,6 +25,7 @@ import
     FormMessage
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { AlertModal } from "@/components/modals/alert-modal";
 
 interface SettingsFormProps{
     initialData: Store;
@@ -37,6 +41,9 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => 
 {
+    const params = useParams()
+    const router = useRouter()
+
     const [open, setOpen] = useState(false)
 
     const [loading, setLoading] = useState(false)
@@ -48,20 +55,36 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 
     const onSubmit = async (data: SettingsFormValues) =>
     {
-        console.log(data)
+        try {
+            setLoading(true)
+            await axios.patch(`/api/stores/${params.storeId}`, data);
+            router.refresh();
+            toast.success("Store updated.")
+        } catch (error) {
+            toast.error("Something went wrong.")
+        } finally {
+            setLoading(false)
+        }
     }
 
     return(
         <>
+        <AlertModal 
+            isOpen={open}
+            onClose={() => setOpen(false)}
+            onConfirm={() => {}}
+            loading={loading}
+        />
             <div className="flex items-center justify-between">
                 <Heading 
                     title="Settings"
                     description="Manage store preferences"
                 />
                 <Button
+                    disabled={loading}
                     variant="destructive"
                     size="icon"
-                    onClick={() => {}}
+                    onClick={() => setOpen(true)}
                 >
                     <Trash className="h-4 w-4"/>
                 </Button>
