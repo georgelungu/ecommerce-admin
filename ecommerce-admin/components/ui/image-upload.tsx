3a4a0e3c0501f34993005e9b661e88ecec757511
George Lungu <george.lungu1896@gmail.com>
2023-09-27 11:59:45 +0300

    upload background image feature done

--- ecommerce-admin/components/ui/image-upload.tsx
+++ ecommerce-admin/components/ui/image-upload.tsx
@@ -1,6 +1,11 @@
 "use client"
 
+import Image from "next/image";
 import { useState, useEffect } from "react";
+import { ImagePlus, Trash } from "lucide-react";
+import { CldUploadWidget } from "next-cloudinary"
+
+import { Button } from "@/components/ui/button";
 
 interface ImageUploadProps {
     disabled?: boolean;
@@ -27,7 +32,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     {
         onChange(result.info.secure_url)
     }
-    
+
     if(!isMounted)
     {
         return null;
@@ -35,7 +40,47 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
 
     return(
         <div>
-            ImageUpload
+            <div className="mb-4 flex items-center gap-4" >
+                {value.map((url) => (
+                    <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden"> 
+                        <div className="z-10 absolute top-2 right-2"> 
+                            <Button 
+                                type="button" 
+                                onClick={() => onRemove(url)} 
+                                variant="destructive" 
+                                size="icon"
+                            >
+                                <Trash className="h-4 w-4"/>
+                            </Button>
+                        </div>
+                        <Image 
+                            fill
+                            className="object-cover"
+                            alt="Image"
+                            src={url}
+                        />
+                    </div>
+                ))}
+            </div>
+            <CldUploadWidget onUpload={onUpload} uploadPreset="lvepih1y">
+                {({ open }) => {
+                    const onClick = () =>
+                    {
+                        open()
+                    }
+                    return(
+                        <Button
+                            type="button"
+                            disabled={disabled}
+                            variant="secondary"
+                            onClick={onClick}
+                        >
+                            <ImagePlus className="h-4 w-4 mr-2"/>
+                            Upload an Image
+                        </Button>
+                    )
+                }}
+            </CldUploadWidget>
         </div>
     )
 }
