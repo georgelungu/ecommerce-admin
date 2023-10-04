fb412f9829f4fd42124c123a6258f27984d2fd59
George Lungu <george.lungu1896@gmail.com>
2023-10-04 16:59:48 +0300

    Finished Colors Category and started Products Category Also added Category Model in Prisma

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,48 @@
+// left at 06:31:00
+
+import { format } from "date-fns"
+
+import prismadb from "@/lib/prismadb"
+
+import { BillboardClient } from "./components/client"
+import { BillboardColumn } from "./components/columns"
+
+const ProductsPage = async ({
+    params
+}: {
+    params: { storeId: string }
+}) =>
+{
+    const products = await prismadb.product.findMany({
+        where: {
+            storeId: params.storeId
+        },
+        include: {
+            category: true,
+            size: true,
+            color: true,
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
+    const formattedProducts: ProductColumn[] = products.map((item) => ({
+        id: item.id,
+        name: item.name,
+        isFeatured: item.isFeatured,
+        isArchived: item.isArchived,
+        price: item.price,
+        createdAt: format(item.createdAt, "MMMM do, yyyy")
+    }))
+
+    return(
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <BillboardClient data={formattedProducts}/>
+            </div>
+        </div>
+    )
+}
+
+export default ProductsPage
